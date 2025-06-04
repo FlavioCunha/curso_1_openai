ef065c0dcbf8bdd2739d6e9cb1d5f340f7736042
Flavio Cunha <fcunha.gm@gmail.com>
2025-06-04 11:23:23 -0300

    Update tokenization examples for gpt-4 and gpt-3.5 models

--- Alura/Contador_tokens.py
+++ Alura/Contador_tokens.py
@@ -3,18 +3,19 @@ import tiktoken
 
 modelo = "gpt-4"
 codificador = tiktoken.encoding_for_model(modelo)
-lista_tokens = codificador.encode("Você é um categorizador de produtos.")
 
-print("Lista de Tokens: ", lista_tokens)
-print("Quantos tokens temos: ", len(lista_tokens))
-print(f"Custo para o modelo {modelo} é de ${(len(lista_tokens)/1000)*0.03}")
+# Texto processado (tokenizado) via encode
+lista_tokens = codificador.encode("O gato correu.")
 
+print("gpt-4 Lista de Tokens: ", lista_tokens) # Lista de Tokens:  [46, 342, 4428, 12603, 84, 13]
+print("gpt-4 Quantos tokens temos: ", len(lista_tokens))
+print(f"gpt-4 Custo para o modelo {modelo} é de ${((len(lista_tokens)/1000)*0.03):.5f}")
 
 
 modelo = "gpt-3.5-turbo-1106"
 codificador = tiktoken.encoding_for_model(modelo)
-lista_tokens = codificador.encode("Você é um categorizador de produtos.")
+lista_tokens = codificador.encode("O gato pulou.")
 
-print("Lista de Tokens: ", lista_tokens)
-print("Quantos tokens temos: ", len(lista_tokens))
-print(f"Custo para o modelo {modelo} é de ${(len(lista_tokens)/1000)*0.002}")
+print("gpt-3.5 Lista de Tokens: ", lista_tokens) #  Lista de Tokens:  [46, 342, 4428, 7893, 283, 13] -> tokens semelhantes porque as frases são parecidas
+print("gpt-3.5 Quantos tokens temos: ", len(lista_tokens))
+print(f"gpt-3.5 Custo para o modelo {modelo} é de ${((len(lista_tokens)/1000)*0.002):.5f}")
